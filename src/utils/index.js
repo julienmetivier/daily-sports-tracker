78d80eb30a673256f7eb821538c58dc2fa28e98e
Julien M <julienmetivier5@hotmail.com>
2023-10-20 12:48:48 -0400

    Add rank to NCAAF

--- src/utils/index.js
+++ src/utils/index.js
@@ -7,8 +7,11 @@ export function checkIfWinnerExistsAndValue(team) {
 }
 
 export function teamBuilder(team) {
+  // Rule for NCAA (and potentially more)
+  const isRanked = team.curatedRank?.current && team.curatedRank?.current !== 99;
+
   return {
-    name: team.team.displayName,
+    name: isRanked ? `${team.curatedRank.current} - ${team.team.displayName}` : team.team.displayName,
     record: team.records ? team.records[0].summary : 'N/A',
     logo: team.team.logo || null,
     score: team.score >= 0 ? team.score : null,
